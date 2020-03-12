cdeee4767ab65f279b4eb60f0c72dbf1f06a5cfb
Alan Wandke <42473587+awandke@users.noreply.github.com>
2020-03-12 11:26:19 -0500

    coronavirus means I must commit...

--- website/pacecalc.js
+++ website/pacecalc.js
@@ -4,6 +4,7 @@
 document.getElementById("presetcalcbutton").addEventListener("click", calcPreset);
 document.getElementById("customcalcbutton").addEventListener("click", calcCustomPace);
 
+
 function calcPreset(){
     document.getElementById("calculatedpace").innerHTML = "hi";
 
@@ -11,33 +12,86 @@ function calcPreset(){
 }
 
 function calcCustomPace() {
-    var output = "Error"
+    var error = false;
+    var output = "Error";
    
-
     var initialrace = document.getElementById("originaldistance").value;
-    var newrace =  document.getElementById("newdistance").value;
-    var sec = document.getElementById("customseconds").value; 
-    var min = document.getElementById("customminutes").value;
-    var hr = document.getElementById("customhours").value;
+    var initialunits = document.getElementById("customunitsi").value;
+
+    // make sure the units are in meters for easy math
+    if (initialunits == "miles"){
+        initialrace*=1609.34;
+    }
+    else if (initialunits == "meters"){
+        //proceed as normal
+    }
+    else{
+        //sends an error message for bad inputs 
+        error = true;
+    }
     
+    var sec = document.getElementById("customsecondsi").value; 
+    var min = document.getElementById("customminutesi").value;
+    var hr = document.getElementById("customhoursi").value;
+
     var sectotal = parseFloat(hr)*3600 + parseFloat(min)*60 + parseFloat(sec);
-    var pace = sectotal/initialrace;
+    var pace = sectotal/initialrace;//pace in seconds/meter
+    
+    //determine which kind of value to calculate
+    var newrace =  document.getElementById("newdistance").value;
+    var newsec = document.getElementById("customsecondsf").value;
+    var newmin = 
+    var newhr = 
+
+    alert(hi);
+
+    if(newrace == null && (newsec = null || newmin == null || newhr == null)){
+        error = true;
+    }
+    else if (newrace == null){
+        //this will set calcdistance as a default if no boxes are left unfilled
+        output = calcDistance();
+        alert("distance!");
+    }
+    else{
+        output = calcTime();
+        alert("time!");
+    }
+
+
+    if (error){
+        alert("Error: please fill in all appropriate boxes.")
+    }
+    else {
+        document.getElementById("calculatedpace").innerHTML = output;
+    }
+  
+    
+}
+
+function calcDistance(){
+
+
+}
+function calcTime(){
+    var newrace =  document.getElementById("newdistance").value;
     var newtime = pace*newrace;
 
     var newhr = Math.floor(parseFloat(newtime)/3600);
     var newmin = Math.floor(parseFloat(newtime-newhr*3600)/60);
     var newsec = Math.round(parseFloat(newtime-newhr*3600-newmin*60)*100)/100;
 
-    if (newmin<=9){
-        newmin = ""+0+newmin;
-    }
     if (newsec<=9){
         newsec = ""+0+newsec;
     }
 
-    output = "the calculated time is " + newhr +":"+newmin+":"+newsec;
-
-    document.getElementById("calculatedpace").innerHTML = output;
-    
+    if (newhr==0){
+        output = "the calculated time is "+newmin+":"+newsec;
+    }
+    else{
+        if (newmin<=9){
+            newmin = ""+0+newmin;
+        }
+        output = "the calculated time is " + newhr +":"+newmin+":"+newsec;
+    }
 }
-
