233f9e71553a74b592097247df7e21fded9eecda
awandke <awandke@stu.naperville203.org>
2020-04-15 22:09:53 -0500

    finished with first draft of pacecalculator calculations

--- website/pacecalc.js
+++ website/pacecalc.js
@@ -9,6 +9,20 @@ function atest() {
     alert("test");
 }
 
+function secToMinSec(totalseconds){
+    var output = "";
+
+    if (totalseconds <=90){
+        output = totalseconds;
+    }
+    else{
+        var minutes = totalseconds%60;
+        var seconds = totalseconds-(minutes*60);
+        output = parseString(minutes) + ":" + parseString(seconds);
+    }
+
+    return output;
+}
 
 function calcPreset() {
     var error = false;
@@ -25,29 +39,58 @@ function calcPreset() {
     else {
         var seconds = parseFloat(initialmin) * 60 + parseFloat(initialsec);
         var pace = seconds / initialrace; //in seconds per meter
-        var miletime = 1600 * pace; // in seconds
+        var miletime;
+        
+        if (initialrace == 800){
+            miletime = (pace*800+15) *2;       
+        }
+        else if (initialrace == 1600){
+            miletime = pace *1600;
+        }
+        else if (initialrace == 3200){
+            miletime = (pace*1600)-20;
+        }
+        else if (initialrace == 4800){
+            miletime = (pace*1600)-35;
+        }
+        else{
+            error = true;
+        }
         
         var time800 = (miletime-30)/2;
         var twomiletime = (miletime + 20); 
         var threemiletime = (miletime + 35);
         var CVtime = miletime + 45;
         var tempotime = miletime + 60; 
-        var upbeatmin = miletime + 90; 
+        var upbeattime = miletime + 90; 
 
         var mile300 = miletime / 1600 * 300;
         var mile400 = miletime / 1600 * 400;
         var mile600 = miletime / 1600 * 600;
-        output += "mile paces: 300 in " + mile300 + " | 400 in " + mile400 + " | 600 in " + mile600 + ".\n";
+        var milerace = miletime;
+        output += "mile paces: 300 in " + mile300 + " | 400 in " + mile400 + " | 600 in " + mile600 + " | 1600 in "+ milerace +".\n";
         
-        var twomile600 = twomiletime / 1600 * 600;
         var twomile800 = twomiletime / 1600 * 800;
         var twomile1000 = twomiletime / 1600 * 1000;
         var twomile1200 = twomiletime / 1600 * 1200;
-        var twomile1600 = twomiletime / 1600 * 1600;
-        output += "two mile paces (VO2): 600 in " + twomile600 + " | 800 in " + twomile800 + " | 1000 in " + twomile1000 + 
+        var twomile1600 = twomiletime;
+        var twomilerace = twomiletime * 2;
+        output += "two mile paces (VO2): 800 in " + twomile800 + " | 1000 in " + twomile1000 + 
         " | 1200 in " + twomile1200 + " | 1600 in " + twomile1600 +  ".\n"; 
 
+        var threemile1000 = threemiletime / 1600 *1000;
+        var threemile1200 = threemiletime / 1600 * 1200;
+        var threemile1600 = threemiletime; 
+        var threemile2400 = threemiletime * 1.5;
+        var threemilerace = threemiletime * 3;
 
+        var CV800 = CVtime/1600 *800;
+        var CV1000 = CVtime/1600 * 1000;
+        var CV1200 = CVtime/1600 * 1200;
+        var CV1600 = CVtime;
+
+        var tempo1600 = tempotime;
+        var upbeat1600 = upbeattime;
 
     }
 
